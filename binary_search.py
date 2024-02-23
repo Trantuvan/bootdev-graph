fd08fb2972f9e3a793c5f63e6b98ec07745a6135
trantuvan <trantuvan.kan@gmail.com>
2024-02-23 13:56:43 +0700

    add binary_search.py

---
+++ binary_search.py
@@ -0,0 +1,10 @@
+def binary_search(target, arr):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        median = (low + high) // 2
+        if arr[median] < target:
+            low = median + 1
+        else:
+            high = median - 1
+    return low != len(arr) and arr[low] == target
